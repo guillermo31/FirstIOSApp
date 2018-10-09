e3f40bc2663ad207381a0cd62c680d73c1f24194
Guillermo <memo33101@gmail.com>
2018-10-09 08:44:30 -0600

    created model folder and added a file

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Ramos aguirre, Guillermo on 10/9/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
